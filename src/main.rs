c88f6df81ffe0f40b5d0aaaa4c3389e4e6fe5cfb
arshiamufti <a2mufti@uwaterloo.ca>
2017-09-24 22:38:20 +0100

    parse an entire pcap file and feed it to peel-ip

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 extern crate pcap;
 use pcap::Capture;
 use std::path::Path;
+extern crate peel_ip;
+use peel_ip::prelude::*;
 
 fn main() {
-    let p = Path::new("test_pcap.pcapng");
+
+    let mut peel = PeelIp::default();
+    peel.set_log_level(LogLevel::Trace);
+    let p = Path::new("httpbin_pcap.pcapng");
     let mut cap = Capture::from_file(&p).unwrap();
-
+    // let limit = 5;
+    let mut count = 0;
     println!("beginning pcap file parse");
-
-        while let Ok(packet) = cap.next() {
-            println!("received packet! {:?}", packet);
-        }
+    while let Ok(packet) = cap.next() {
+        println!("traversing a packet....");
+        println!("{:?}", packet.data);
+        peel.traverse(&packet.data, vec![]).result;
+        // count = count + 1;
+        // if count > limit {
+        //     break;
+        // }
+    }
     println!("end of pcap file parse");
 
 }
